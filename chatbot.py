53df35b799a8d2003faa5edcefe07875aa17d193
anaismonet <58906560+anaismonet@users.noreply.github.com>
2020-09-24 11:29:13 +0200

    liste messages

--- chatbot.py
+++ chatbot.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+messages = []
+
 @app.route('/', methods=["POST", "GET"])
 def index():
+    global messages 
     message = request.form.get("message_input")
+    messages.append(message)
+    print(messages)
     print(message)
-    return render_template('index.html', message=message)
+    
+    return render_template('index.html', message = messages)
 
 if __name__ == "__main__":
     app.run()
