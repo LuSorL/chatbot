a1ba5f51cc1484972701fcfd86282822df317bf0
louis morel <louismorel6@gmail.com>
2020-09-24 11:55:07 +0200

    lance script + css

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import test
 
 app = Flask(__name__)
 
@@ -6,12 +7,13 @@ messages = []
 
 @app.route('/', methods=["POST", "GET"])
 def index():
-    global messages 
+    global messages
+    test.test()
     message = request.form.get("message_input")
     messages.append(message)
     print(messages)
     print(message)
-    
+
     return render_template('index.html', message = messages)
 
 if __name__ == "__main__":
---
+++ test.py
@@ -0,0 +1,3 @@
+
+def test():
+    print("ca marche")
