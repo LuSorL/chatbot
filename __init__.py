53a794b62fcc4e9463d5e6159c411dc0cdd5d4d4
louis morel <louismorel6@gmail.com>
2020-09-24 16:46:04 +0200

    fix some problems

--- __init__.py
+++ __init__.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template, request
-from .model import init, prediction_msg
 
 
-#def create_app():
+
 app = Flask(__name__)
-classifier = init()
-    #return app
 
 
 import chatbot.chat
--- chat.py
+++ chat.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 from .model import init, prediction_msg
-#import .
 from chatbot import app
 messages = []
 reponses = []
+
+#classifier = init()
 
 @app.route('/')
 def index():
@@ -14,7 +15,8 @@ def index_msg():
     global messages, reponses
     message = request.form.get("message_input")
     messages.append(message)
-    attente = prediction_msg(message)
+    #attente = prediction_msg(message, classifier)
+    attente = ['autre']
     if attente[0] == 'autre':
         reponses.append('Je ne suis pas capable de répondre à votre demande')
     else :
