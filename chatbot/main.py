46093ebe79474f87151dcb1c385932f8b53b1a7f
anaismonet <58906560+anaismonet@users.noreply.github.com>
2020-09-30 18:46:51 +0200

    c'est fini

--- chatbot/main.py
+++ chatbot/main.py
@@ -55,6 +55,7 @@ def chat():
             found = 1
             break
 
+    print("found", found)
     if found == 0 :
         with open(cwd + "calendrier.json","w") as f :
                 json.dump([],f)
@@ -63,6 +64,7 @@ def chat():
     json_file = json.load(file)
     file.close()
 
+    print("json_file", json_file)
     return render_template('index.html', message = messages, reponse = reponses, pseudo = pseudo )
 
 @main.route('/chatbot', methods=["GET"])
@@ -105,31 +107,125 @@ def chat_msg():
         #calcul timing
         pages = int(nb_pages)
 
-        if len(json_file) == 0:
-            start = time
-        else:
-            if (datetime.strptime(day + " 19:00:00", "%m/%d/%Y %H:%M:%S") -  date).seconds > pages and datetime.strptime(day + " 19:00:00", "%m/%d/%Y %H:%M:%S") > date :
-                late = datetime.strptime(json_file[-1]["end"], "%m/%d/%Y %H:%M:%S")
-                if (date - late).days == 0:
-                    start = max(date, late).strftime("%m/%d/%Y %H:%M:%S")
-                else:
-                    for i in range(len(json_file), 0, -1):
-                        elem = datetime.strptime(json_file[i-1]['end'], "%m/%d/%Y %H:%M:%S")
-                        if (elem.day == date.day):
-                            if (elem - elem.replace(hour = 19, minute = 00, second = 00)).seconds > pages:
-                                start = elem.strftime("%m/%d/%Y %H:%M:%S")
-                                break
+
+        while True : 
+            br = False
+            if len(json_file) == 0:
+                print("Le json file est vide")
+                start = time
+                break
             else:
-                late = datetime.strptime(json_file[-1]["end"], "%m/%d/%Y %H:%M:%S")
-                if (date - late).days == 0:
-                    day_after = date.day +1
-                    start = date.replace(day = day_after, hour = 7, minute = 0, second = 0, microsecond = 0)
+                print("Le json file n'est pas vide")
+                print("nom du dernier objet", json_file[-1]['titre'])
+                # Cas 1 : on vérifie entre l'heure actuelle et 19:00:00 si on peut imprimer notre nombre de pages et on vérifie si heure actuelle est bien < 19:00:00
+                if (datetime.strptime(day + " 19:00:00", "%m/%d/%Y %H:%M:%S") -  date).seconds > pages and datetime.strptime(day + " 19:00:00", "%m/%d/%Y %H:%M:%S") > date :
+                    print("Je remplis les conditions du cas 1")
+                    late = datetime.strptime(json_file[-1]["end"], "%m/%d/%Y %H:%M:%S")
+                    print("Je suis le dernier élément du fichier calendrier.json", late)
+
+                    #Sous cas 1 : si aujourd'hui et le dernier élément du json ont le même jour
+                    if (date - late).days == -1:
+                        print("Je remplis les conditions du sous cas 1 : date et late sont le même jour")
+
+                        #Sous sous cas 1 : on vérifie si entre le dernier élément et 19:00:00 il y a le temps d'imprimer nos pages et si late est < à 19:00:00
+                        if (datetime.strptime(day + " 19:00:00", "%m/%d/%Y %H:%M:%S") -  late).seconds > pages and datetime.strptime(late.strftime("%m/%d/%Y") + " 19:00:00", "%m/%d/%Y %H:%M:%S") > late :
+                            print("Je remplis les conditions du sous sous cas 1")
+                            start = max(date, late).strftime("%m/%d/%Y %H:%M:%S")
+                            print("start", start)
+                            break
+
+                        #Sous sous cas 2 : 
+                        else:
+                            print("je remplis sous sous cas 2")
+                            day_after = late.day +1
+                            print("day_after", day_after)
+                            try:
+                                start = date.replace(day = day_after, hour = 7, minute = 0, second = 0, microsecond = 0 ).strftime("%m/%d/%Y %H:%M:%S")
+                                break
+                            except ValueError:
+                                start = date.replace(day = 1, month = date.month +1, hour = 7, minute = 0, second = 0, microsecond = 0 ).strftime("%m/%d/%Y %H:%M:%S")
+                                break
+                    # Sous cas 2 : 
+                    else:
+                        print("Je suis dans le cas 2")
+                        for i in range(len(json_file), 0, -1):
+                            print("elem numéro ", i)
+                            elem = datetime.strptime(json_file[i-1]['end'], "%m/%d/%Y %H:%M:%S")
+                            print("elem du cas 2", elem)
+                            if (elem.day == date.day):
+                                if (elem - elem.replace(hour = 19, minute = 00, second = 00)).seconds > pages:
+                                    start = elem.strftime("%m/%d/%Y %H:%M:%S")
+                                    br = True
+                                    break
+                                else :
+                                    date = late
+                                    day = late.strftime("%m/%d/%Y")
+                                    print("Ce n'est pas la même jour")
+                                    break
+                        if br:           
+                            break
+                # Cas 2
                 else:
-                    start = late.replace(second = late.seconds + 1)
+                    late = datetime.strptime(json_file[-1]["end"], "%m/%d/%Y %H:%M:%S")
+                    print("late du Cas 2", late)
+                    print("date du Cas 2", date)
+                    # 2.1
+                    if (date - late).days == 0:
+                        print('dif ', (date - late).days)
+                        day_after = date.day +1
+                        try:
+                            print("2.1 premier try ")
+                            start = date.replace(day = day_after, hour = 7, minute = 0, second = 0, microsecond = 0 ).strftime("%m/%d/%Y %H:%M:%S")
+                            print("date 2.1 premier try", start)
+                            break
+                        except ValueError:
+                            print("2.1 valueError")
+                            start = date.replace(day = 1, month = date.month +1, hour = 7, minute = 0, second = 0, microsecond = 0 ).strftime("%m/%d/%Y %H:%M:%S")
+                            print("start 2.1 value Error", start)
+                            break
+                    #2.2
+                    else:
+                        #2.2.1
+                        if (datetime.strptime(late.strftime("%m/%d/%Y") + " 19:00:00", "%m/%d/%Y %H:%M:%S") -  late).seconds > pages and datetime.strptime(late.strftime("%m/%d/%Y") + " 19:00:00", "%m/%d/%Y %H:%M:%S") > late :
+                            start = late.strftime("%m/%d/%Y %H:%M:%S")
+                            print("start 2.2.1", start)
+                            break
+                        #2.2.2
+                        else:
+                            day_after = late.day +1
+                            start = late.replace(day = day_after, hour = 7, minute = 0, second = 0, microsecond = 0 ).strftime("%m/%d/%Y %H:%M:%S")
+                            print("start 2.2.2", start)
+                            break
 
 
+        start = datetime.strptime(start, "%m/%d/%Y %H:%M:%S")
+        # fin de semaine
+        if start.isoweekday() == 7:
+            try:
+                start = start.replace(day = start.day + 1)
+                print("fin de semaine dimanche try", start)
+            except ValueError:
+                start = start.replace(day = 1, month = start.month +1)
+                print("fin de semaine dimanche except", start)
+        if start.isoweekday() == 6:
+            try:
+                start = start.replace(day = start.day + 1)
+                print("fin de semaine samedi start premier try:", start)
+                try:
+                    start = start.replace(day = start.day + 1)
+                    print("fin de semaine samedi start deuxième try:", start)
+                except ValueError:
+                    start = start.replace(day = 1, month = start.month +1)
+                    print("fin de semaine samedi start valueerror dans le premier try:", start)
+                
+            except ValueError:
+                start = start.replace(day = 2, month = start.month +1)
+                print("fin de semaine samedi start valueerror:", start)
+
+        start = start.strftime("%m/%d/%Y %H:%M:%S")
         end = datetime.strptime(start, "%m/%d/%Y %H:%M:%S") + timedelta(seconds = pages)
-
+    
+            
         #Update json
         object = {}
         object['titre'] = nom_doc
@@ -142,6 +238,8 @@ def chat_msg():
         json_file.append(object)
 
         #ecriture JSON
+        print(json_file)
+        cwd = os.getcwd() + '/chatbot/static/'
         file = open(cwd + "calendrier.json", "w")
         json.dump(json_file, file)
         file.close()
@@ -153,7 +251,6 @@ def chat_msg():
 
 @main.route('/calendrier', methods=['POST','GET'])
 def calendrier():
-    print("json ", json_file)
     week = ['Lundi','Mardi','Mercredi','Jeudi','Vendredi']
     return render_template('calendrier.html', json = json_file, week = week)
 
